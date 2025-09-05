93d333cee99d451fecf8aa86040f9536ebce8494
Benjamin Labrecque <labrecque.benji@gmail.com>
2025-09-05 17:08:36 +0200

    Show and image and its label

--- src/mnist/main.py
+++ src/mnist/main.py
@@ -2,10 +2,15 @@ import torchvision
 
 
 def main():
-    torchvision.datasets.MNIST(
+    mnist = torchvision.datasets.MNIST(
         root="data",
         download=True,
     )
+
+    for image, label in mnist:
+        image.show()
+        print(label)
+        break
 
 
 if __name__ == "__main__":
